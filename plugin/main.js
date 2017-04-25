74b2a2bca610d0fcb40c01d50e2a51095a5bd373
Waleed OMEGA <waleedomega@hotmail.com>
2017-04-25 18:16:39 +0200

    responsive nav

--- plugin/main.js
+++ plugin/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+    var clicked = false;
     /* nav button click event */
     $('header nav .nav-btn').click(function(){
-        $(this).parent().toggleClass('active');
+        var $this = $(this);
+        if(!clicked){
+        clicked=true;
+        $this.parent().toggleClass('active');
+            setTimeout(function(){
+                clicked=false
+            },2250)
+           }
+        
     });
 });

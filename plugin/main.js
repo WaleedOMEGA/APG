e98a46598fba144cc0db4a2747d1c003c4fcd87a
Waleed OMEGA <waleedomega@hotmail.com>
2020-08-05 13:25:46 +0200

    Add files via upload

--- plugin/main.js
+++ plugin/main.js
@@ -27,5 +27,77 @@ $(document).ready(function(){
             $header.removeClass('sticky');
         }
     });
-    console.log($mainsection.height());
+    $("html").niceScroll({
+        cursorcolor: "#0079BC",
+        cursoropacitymin: 0.5,
+        cursorwidth: "5px",
+        cursorborder: "1px solid #263034"
+    });
+    var nameerror = true,
+        companyerror = true,
+        emailerror=true,
+        mobileerror=true,
+        msgerror=true;//setting error status
+    $('.name').blur(function(){
+    if($(this).val().length < 4){
+        $(this).css('border','1px solid #f00');
+        $(this).parent().find('.custom-alert').fadeIn(300);
+        nameerror=true;
+    }else{
+        $(this).css('border','1px solid #0f0');
+        $(this).parent().find('.custom-alert').fadeOut(300);
+        nameerror=false;
+    }
+    });
+    $('.company-name').blur(function(){
+        if($(this).val() === ''){
+            $(this).css('border','1px solid #f00');
+            $(this).parent().find('.custom-alert').fadeIn(300);
+            companyerror=true;
+        }else{
+            $(this).css('border','1px solid #0f0');
+            $(this).parent().find('.custom-alert').fadeOut(300);
+            companyerror=true;
+        }
+    });
+    $('.email').blur(function(){
+        if($(this).val() === ''){
+            $(this).css('border','1px solid #f00');
+            $(this).parent().find('.custom-alert').fadeIn(300);
+            emailerror=true;
+        }else{
+            $(this).css('border','1px solid #0f0');
+            $(this).parent().find('.custom-alert').fadeOut(300);
+            emailerror=false;
+        }
+    });
+    $('.mobile').blur(function(){
+        if($(this).val() === ''){
+            $(this).css('border','1px solid #f00');
+            $(this).parent().find('.custom-alert').fadeIn(300);
+            mobileerror=true;
+        }else{
+            $(this).css('border','1px solid #0f0');
+            $(this).parent().find('.custom-alert').fadeOut(300);
+            mobileerror=false;
+        }
+    });
+    $('.msg').blur(function(){
+        if($(this).val().length < 11){
+            $(this).css('border','1px solid #f00');
+            $(this).parent().find('.custom-alert').fadeIn(300);
+            msgerror=true;
+        }else{
+            $(this).css('border','1px solid #0f0');
+            $(this).parent().find('.custom-alert').fadeOut(300);
+            msgerror=false;
+        }
+    }); 
+    // submit form validation
+    $('form').submit(function(e){
+		if (nameerror=== true || companyerror === true || emailerror === true || mobileerror === true || msgerror === true){
+            e.preventDefault();
+        $('.name, .company-name, .email, .mobile, .msg').blur();
+        }
+    });
 });
